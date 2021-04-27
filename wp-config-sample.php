02fa3c5f43a3d2066eeff119026f5009e8b080b5
Shibra Amin <shibraamin18@gmail.com>
2021-04-27 14:31:20 +0530

    Update wp-config-sample.php

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -85,6 +85,7 @@ define( 'WP_DEBUG', false );
 if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', __DIR__ . '/' );
 }
-
+define('WP_SITEURL', 'https://' . $_SERVER['HTTP_HOST']);
+define('WP_HOME', 'https://' . $_SERVER['HTTP_HOST']);
 /** Sets up WordPress vars and included files. */
 require_once ABSPATH . 'wp-settings.php';
